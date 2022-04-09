89c9ddfc942d202dc16428583ddbfa2a22a9d258
Samaksh-Rawat <97016925+Samaksh-Rawat@users.noreply.github.com>
2022-04-09 15:41:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+student_array=[];
+function submit()
+{
+var n1=document.getElementById("n1").value;
+var n2=document.getElementById("n2").value;
+var n3=document.getElementById("n3").value;
+var n4=document.getElementById("n4").value;
+student_array.push(n1);
+student_array.push(n2);
+student_array.push(n3);
+student_array.push(n4);
+console.log(student_array);
+document.getElementById("display").innerHTML=student_array;
+document.getElementById("submit_button").style.display="none";
+document.getElementById("sort_button").style.display="inline-block";
+}
+function sort()
+{
+    student_array.sort();
+    console.log(student_array);
+    document.getElementById("display").innerHTML=student_array;
+}
