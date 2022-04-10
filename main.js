27bdf3c14a3fd4c0d737352489f95e496948b78f
Samaksh-Rawat <97016925+Samaksh-Rawat@users.noreply.github.com>
2022-04-10 15:44:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
 student_array=[];
 function submit()
 {
-var n1=document.getElementById("n1").value;
-var n2=document.getElementById("n2").value;
-var n3=document.getElementById("n3").value;
-var n4=document.getElementById("n4").value;
-student_array.push(n1);
-student_array.push(n2);
-student_array.push(n3);
-student_array.push(n4);
+var display_student_array=[];
+for(var j=1;j<=4;j++)
+{
+var name_student=document.getElementById("name_of_the_student_"+j).value;
+console.log(name_student);
+student_array.push(name_student);
+}
 console.log(student_array);
-document.getElementById("display").innerHTML=student_array;
+var length_array=student_array.length;
+console.log(length_array);
+
+for(var k=0;k<length_array;k++)
+{
+display_student_array.push("<h4>NAME- "+ student_array[k] + "</h4>");
+console.log(display_student_array);
+}
+console.log(display_student_array);
+document.getElementById("display_name_with_commas").innerHTML=display_student_array;
+var remove_comma=display_student_array.join(" ");
+console.log(remove_comma);
+document.getElementById("display_name_without_commas").innerHTML=remove_comma;
 document.getElementById("submit_button").style.display="none";
 document.getElementById("sort_button").style.display="inline-block";
 }
-function sort()
+function sorting()
 {
-    student_array.sort();
-    console.log(student_array);
-    document.getElementById("display").innerHTML=student_array;
+student_array.sort();
+console.log(student_array);
+display_student_array_sorting=[];
+var length_array=student_array.length;
+console.log(length_array);
+for(var k=0;k<length_array;k++)
+{
+display_student_array_sorting.push("<h4>NAME- "+ student_array[k] + "</h4>");
+console.log(display_student_array_sorting);
+}
+var remove_comma=display_student_array_sorting.join(" ");
+console.log(remove_comma);
+document.getElementById("display_name_without_commas").innerHTML=remove_comma;
 }
